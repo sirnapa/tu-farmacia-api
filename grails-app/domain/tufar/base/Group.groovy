bd5dde8b0ebbe6c84606c6c56a9f6722cd3f761d
Nelson Paez <nelpa90@gmail.com>
2017-11-24 17:54:02 -0300

    Dominios de Group y Store

---
+++ grails-app/domain/tufar/base/Group.groovy
@@ -0,0 +1,26 @@
+package tufar.base
+
+import tufar.util.PaginableController
+import grails.rest.*
+
+@Resource(uri='/api/groups', readOnly = false, formats = ['json', 'xml'], superClass = PaginableController)
+class Group {
+
+    String name
+    String ruc
+    String address
+    String phone
+    String email
+
+    static hasMany = [stores: Store]
+
+    static constraints = {
+        name blank: false, unique: true
+        ruc blank: false, unique: true
+    }
+
+    static mapping = {
+		table name:"group", schema:"base"
+		id generator:'sequence', params:[sequence:'security.seq_group_id']
+	}
+}
---
+++ grails-app/domain/tufar/base/Store.groovy
@@ -0,0 +1,26 @@
+package tufar.base
+
+import tufar.util.PaginableController
+import grails.rest.*
+
+@Resource(uri='/api/stores', readOnly = false, formats = ['json', 'xml'], superClass = PaginableController)
+class Store {
+
+    String name
+    String address
+    String phone
+    String email
+    Double latitude
+    Double longitude
+
+    static belongsTo = [group: Group]
+
+    static constraints = {
+        name blank: false, unique: true
+    }
+
+    static mapping = {
+		table name:"store", schema:"base"
+		id generator:'sequence', params:[sequence:'security.seq_store_id']
+	}
+}
